75d7f1dbf09ed09f7d56fe6383e5c031962647d3
임재현 <ijh0116@widebrain.kr>
2022-09-07 18:22:16 +0900

    third person controll

---
+++ Assets/3.Scripts/Character/ThirdPersonController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackTree
+{
+    public class ThirdPersonController : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+
+}
